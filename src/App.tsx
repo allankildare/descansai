2dee0d25daff5e43851e6ab22bfc59991b83bd4d
Allan Kildare <allankildare27@gmail.com>
2021-02-26 00:17:27 -0300

    + dia 1 concluido

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ExperienceBar } from "./components/ExperienceBar/ExperienceBar";
+import "./App.css"
+function App() {
+  return (
+      <div className="container">
+        <ExperienceBar/>
+      </div>
+  )
+}
+
+export default App
